f8323e44e7430f4f34d1d55409feeb9f949e2298
robert <robertomarcial688@gmail.com>
2021-10-15 22:26:09 -0500

    Se agrego el metodo editar en el controlador libros

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,9 +44,32 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    #Busca primero el dato
+    @book = Book.find(params[:id])
+
+    #Recoge los datos recibidos mediante un hash
+    datos_libro = params[:book]
+
+
+    titulo_lib = datos_libro[:titulo]
+    idioma_lib = datos_libro[:idioma]
+    paginas_lib = datos_libro[:paginas]
+    descripcion_lib = datos_libro[:descripcion]
+    autor_lib = datos_libro[:author_id]
+    categoria_lib = datos_libro[:category_id]
+    editorial_lib = datos_libro[:editorial_id]
+
+    if @book.update(titulo: titulo_lib, idioma: idioma_lib, paginas: paginas_lib, descripcion: descripcion_lib, author_id: autor_lib, category_id: categoria_lib, editorial_id: editorial_lib)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+
+
   end
 
   def delete
