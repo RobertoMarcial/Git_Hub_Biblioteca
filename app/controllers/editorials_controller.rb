4d2f1004cb7bb79c0c4b45f98bb7a5018f39c932
robert <robertomarcial688@gmail.com>
2021-10-17 00:49:17 -0500

    Creacion del crud en editorials

--- app/controllers/editorials_controller.rb
+++ app/controllers/editorials_controller.rb
@@ -21,16 +21,36 @@ class EditorialsController < ApplicationController
     @editorial = Editorial.new(nombre: nom_editorial, pais: pais_editorial, telefono: tel_editorial)
 
     @editorial.save
-    redirect_to editorial_path
+    redirect_to editorial_path(@editorial)
 
   end
 
   def edit
+      @editorial = Editorial.find(params[:id])
   end
 
   def update
-  end
+    @editorial = Editorial.find(params[:id])
 
-  def delete
+    datos_editorial = params[:editorial]
+
+    nom_editorial = datos_editorial[:nombre]
+    pais_editorial = datos_editorial[:pais]
+    tel_editorial = datos_editorial[:telefono]
+
+    if @editorial.update(nombre: nom_editorial, pais: pais_editorial, telefono: tel_editorial)
+        redirect_to editorial_path(@editorial)
+    else
+      render :edit
+    end
+
+
+  end
+#NO se que vergas hice con esto pero ya funciona me daba un error al momento de elimanar el dato ya que la tabla books contenia
+#el id de editorial pero acomode en el modelo editorial esto :dependent => :destroy y le puse destroy en @editorial.destroy :v
+  def destroy
+    @editorial = Editorial.find(params[:id])
+    @editorial.destroy
+    redirect_to editorials_path
   end
 end
